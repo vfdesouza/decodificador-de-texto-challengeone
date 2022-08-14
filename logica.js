c26b6dac4e7b22d596953e5fe9c32bfa23e8cb46
vfdesouza <vfdesouza@outlook.com>
2022-08-14 13:14:12 -0300

    step 1: implementing the logic

---
+++ logica.js
@@ -0,0 +1,38 @@
+const output = document.getElementById("output");
+const btnCripto = document.getElementById("btn-cripto");
+const btnDescripto = document.getElementById("btn-descripto");
+const btnCopiar = document.getElementById("btn-copiar");
+
+btnCripto.onclick = cripto;
+btnDescripto.onclick = descripto;
+btnCopiar.onclick = copiar;
+
+function cripto() {
+    var input = document.getElementById("input").value;
+    var result = input
+                    .replace(/e/gi, "enter")
+                    .replace(/i/gi, "imes")
+                    .replace(/a/gi, "ai")
+                    .replace(/o/gi, "ober")
+                    .replace(/u/gi, "ufat");
+
+    output.value = result.toLowerCase();
+}
+
+function descripto() {
+    var input = document.getElementById("input").value;
+    var result = input                    
+                    .replace(/enter/gi, "e")
+                    .replace(/imes/gi, "i")
+                    .replace(/ai/gi, "a")
+                    .replace(/ober/gi, "o")
+                    .replace(/ufat/gi, "u");
+
+    output.value = result.toLowerCase();
+}
+
+function copiar() {
+    var input = document.getElementById("input").value;
+    navigator.clipboard.writeText(input);
+    alert("Texto copiado!");
+}
